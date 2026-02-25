6d8c5ea34841b915049d9e42007dcd82aa9d4761
Edih Goodluck <goodluckedih@yahoo.com>
2026-02-25 15:06:00 +0100

    removed revalidation

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,7 +4,6 @@ import { Blogs, ReadyToMove } from "../components";
 import { ContentfulProvider } from "@/services";
 import { Suspense } from "react";
 
-export const revalidate = 300;
 
 export default async function Page() {
   const blogs = await ContentfulProvider.getBlogEntries();
