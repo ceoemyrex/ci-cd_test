ba37144deb7e6f5ea25329b95d69b764fe9775db
Edih Goodluck <goodluckedih@yahoo.com>
2026-03-01 12:58:47 +0100

    major cleanup on all pages

--- app/track-move/components/Hero.tsx
+++ app/track-move/components/Hero.tsx
@@ -47,11 +47,6 @@ export function TrackMoveHero() {
         <div className="relative max-w-310  2xl:max-w-350 mx-auto p-4">
           <Navbar />
           <div className="pb-12.5 lg:pb-37.5 pt-20 lg:pt-30">
-            {errorMessage && (
-              <div className="bg-red-100 p-2 lg:p-4 rounded-lg border border-red-200 text-red-400">
-                {errorMessage}
-              </div>
-            )}
             <header className="flex items-center">
               <div className="text-grey flex items-center text-sm lg:text-xl">
                 <p>Home</p>
@@ -64,6 +59,11 @@ export function TrackMoveHero() {
               </p>
             </header>
             <div className="mt-8 lg:mt-20 bg-white/50 p-4 space-y-8 rounded-2xl border border-white max-w-175.5 mx-auto">
+             {errorMessage && (
+              <div className="bg-red-100 p-2 lg:p-4 rounded-lg border border-red-200 text-red-400">
+                {errorMessage}
+              </div>
+            )}
               <div className="h-40 lg:h-60 relative">
                 <img
                   src="/images/map.png"
